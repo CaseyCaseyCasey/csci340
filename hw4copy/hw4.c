89a2f04969fda4ee07e7651fca892fa21b492841
Team Casey <Casey@macaseys-mbp-3.wireless.cougars.int>
2016-02-18 15:24:53 -0500

    first commit

---
+++ hw4copy/hw4.c
@@ -0,0 +1,80 @@
+/*
+----------------------------------------------
+These are the only libraries that can be used. Under no circumstances
+can additional libraries be included.
+----------------------------------------------
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include "shell.h"
+
+/*
+-----------------------------------
+  Main function 
+  Arguments:	argc = number of arguments suppled by user
+                argv = array of argument values
+-----------------------------------
+*/
+
+static void sig_child_handler( int sig );
+
+int main( int argc, char** argv ) {
+  command_t cmd;
+  char line[MAXSTRLEN];
+  char fullpath[MAXSTRLEN];
+  int done = FALSE;
+
+  while (!done) {
+    printf(">> ");
+    fgets(line, MAXSTRLEN, stdin);
+    line[my_strlen(line)-1] = '\0'; // get rid of newline
+    parse(line, &cmd);
+
+    if (cmd.argc > 0) {   // fix for no command (ie white space) entered
+      if (my_strequal(cmd.name, "exit")) {
+	done = TRUE;
+      }
+      else if (is_builtin(&cmd)) {
+	do_builtin(&cmd);
+      }
+      else if (find_fullpath(fullpath, &cmd)) {
+	// NOTE: find_fullpath() is called again in execute
+          //add handlers here
+          if(signal(SIGCHLD,sig_child_handler)== SIG_ERR){
+              perror("SIGCHLD GEN. FAILED\n");
+              exit(-1);
+          }
+	    execute(&cmd);
+
+      }
+      else {
+	  printf("invalid command\n");
+      }
+    }
+    
+    cleanup(&cmd);
+  }
+  
+  return 0;
+} // end main function
+
+
+static void sig_child_handler( int sig ) {
+
+  printf("In SIGCHLD handler\n");
+  int status;
+  pid_t pid;
+    wait(sig);
+ /*
+  while ((pid = waitpid(-1, &status, WNOHANG)) != -1) {
+      if(pid !=0) {
+          printf("Child Process (%d) has Terminated\n", pid);
+      }
+  }
+  */
+
+}
